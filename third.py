de8a056e4cf7b02bbdb3328e4ee13236b373bd94
skorapkaa <158566510+skorapkaa@users.noreply.github.com>
2024-10-23 16:58:57 +0200

    3. ukol

--- third.py
+++ third.py
@@ -1,1 +1,26 @@
+import math
+
+def je_prvocislo(cislo):
+
+    if cislo <= 1:
+        return False
+    for i in range(2, int(math.sqrt(cislo)) + 1):
+        if cislo % i == 0:
+            return False
+    return True
+
+def vrat_prvocisla(maximum):
+
+    seznam_prvocisel = []
+    for cislo in range(2, maximum + 1):
+        if je_prvocislo(cislo):
+            seznam_prvocisel.append(cislo)
+    return seznam_prvocisel
+
+if __name__ == "__main__":
+    cislo = int(input("Zadej maximum: "))
+    prvocisla = vrat_prvocisla(cislo)
+    print(prvocisla)
+    cislo2 = int(input("Zadej číslo: "))
+    print(je_prvocislo(cislo2))
 
