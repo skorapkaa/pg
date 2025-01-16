29cabe39bc416722eee9de405d7b15b19e4fedfa
skorapka <158566510+skorapkaa@users.noreply.github.com>
2025-01-16 11:35:28 +0100

    Create z3.py

---
+++ z3.py
@@ -0,0 +1,64 @@
+# Příklad 3: Základy OOP (dědičnost, abstrakce, zapouzdření)
+# Zadání:
+# Vytvořte dvě podtřídy třídy `Shape`: `Rectangle` a `Circle`.
+# - `Rectangle` má atributy `width` a `height` a implementuje metodu `area`, která spočítá plochu obdelníku (zaokrouhlenou na 1 desetinné místo).
+# - `Circle` má atribut `radius` a implementuje metodu `area`, která spočítá plochu kruhu (zaokrouhlenou na 1 desetinné místo).
+# - ve třídě `Circle` navíc implementujte metodu `__str__`, která vrátí řetězec ve tvaru `{self.shape_name} with a radius of {self.radius} has an area of {self.area}`.
+
+
+class Shape():
+
+    def __init__(self, shape_name=None):
+        self.shape_name = shape_name
+    
+    def __str__(self):
+        return f'{self.shape_name} shape with area {self.area()}'
+
+    def area(self):
+        return 0.0
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        super().__init__("Rectangle")
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return float(round(self.width * self.height, 1))
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+
+    def area(self):
+        return float(round(3.14 * (self.radius ** 2), 1))
+
+    def __str__(self):
+        return f'{self.shape_name} shape with a radius of {self.radius} has an area of {self.area()}'
+
+
+# ZDE NAPIŠTE VÁŠ KÓD
+
+
+from unittest.mock import patch, MagicMock, mock_open
+
+
+# Unit testy
+def test_shapes():
+    rect = Rectangle(4, 5)
+    assert rect.area() == 20.0
+    assert str(rect) == "Rectangle shape with area 20.0"
+
+    circle = Circle(3)
+    assert round(circle.area(), 1) == 28.3
+    assert str(circle) == "Circle shape with a radius of 3 has an area of 28.3"
+
+
+if __name__ == "__main__":
+    shape = Shape()
+    print(shape)
+    rect = Rectangle(4, 5)
+    print(rect)
+    circle = Circle(3)
+    print(circle)
